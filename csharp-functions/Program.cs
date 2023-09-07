16621d633ce52ab87b5d96a9cd1703b42ecda722
Gaspare <Gas9220@users.noreply.github.com>
2023-09-07 14:42:03 +0200

    Squared array

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -5,6 +5,14 @@ int[] numbers = { 2, 6, 7, 5, 3, 9 };
 Console.Write("Numbers array: ");
 PrintArray(numbers);
 
+Console.WriteLine();
+
+// Square all values of numbers array and verify the original array is untouched
+Console.Write("Squared array: ");
+PrintArray(SquareArray(numbers));
+Console.Write("Untouched original array: ");
+PrintArray((numbers));
+
 // Functions
 
 // Function to print an array
@@ -32,4 +40,17 @@ void PrintArray(int[] array)
 int Square(int number)
 {
     return number * number;
+}
+
+// Function to square all values in the array
+int[] SquareArray(int[] array)
+{
+    int[] newArray = (int[])array.Clone();
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = Square(newArray[i]);
+    }
+
+    return newArray;
 }
