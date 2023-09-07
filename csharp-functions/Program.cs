00f1058bff689f4301b2fbda6fa67df80aba9f51
Gaspare <Gas9220@users.noreply.github.com>
2023-09-07 14:24:39 +0200

    PrintArray function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// Initial array
+int[] numbers = { 2, 6, 7, 5, 3, 9 };
+
+// Print an array
+Console.Write("Numbers array: ");
+PrintArray(numbers);
+
+// Functions
+
+// Function to print an array
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+    }
+
+    Console.WriteLine("]");
+
+}
